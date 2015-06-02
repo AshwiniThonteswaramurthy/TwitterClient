02ee32c748c9d83a96656dcc030d38e0714a25da
Ashwini Thonteswaramurthy <ashwini.thonteswaramurthy@gmail.com>
2015-06-01 17:10:24 -0700

    adding splash image on startup

--- app/src/main/java/com/codepath/apps/twitterclient/activities/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/activities/LoginActivity.java
@@ -4,56 +4,92 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.codepath.apps.twitterclient.R;
 import com.codepath.apps.twitterclient.helpers.TwitterClient;
 import com.codepath.oauth.OAuthLoginActionBarActivity;
 
-public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
+public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> implements Animation.AnimationListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
-	}
+    private Animation zoomin;
+    private ImageView animatedView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        //No title
+        getSupportActionBar().hide();
 
-	// Inflate the menu; this adds items to the action bar if it is present.
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
+        setContentView(R.layout.activity_login);
 
-	// OAuth authenticated successfully, launch primary authenticated activity
-	// i.e Display application "homepage"
-	@Override
-	public void onLoginSuccess() {
-		 Intent i = new Intent(this, MainActivity.class);
-		 startActivity(i);
-	}
+        animatedView = (ImageView) findViewById(R.id.animatedView);
 
-	// OAuth authentication flow failed, handle the error
-	// i.e Display an error dialog or toast
-	@Override
-	public void onLoginFailure(Exception e) {
-		e.printStackTrace();
-	}
+        animatedView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-	// Click handler method for the button used to start OAuth flow
-	// Uses the client to initiate OAuth authorization
-	// This should be tied to a button used to login
-	public void loginToRest(View view) {
-		getClient().connect();
-	}
+                //login
+                getClient().connect();
+            }
+        });
 
-//	public void doSomeAnimation() {
-//		View animatedView = findViewById(R.id.animatedView);
-//		TranslateAnimation animation = new TranslateAnimation(
-//				Animation.ABSOLUTE, -10,
-//				Animation.ABSOLUTE, -10,
-//				Animation.RELATIVE_TO_SELF, 0.5,
-//				Animation.RELATIVE_TO_PARENT, 0.8);
-//		animatedView.startAnimation(animation);
-//	}
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if(hasFocus){
+            zoomin = AnimationUtils.loadAnimation(this,R.anim.zoom_in);
+            zoomin.setAnimationListener(this);
+            zoomin.setRepeatCount(Animation.INFINITE);
+            animatedView.startAnimation(zoomin);
+        }
+    }
+
+    // Inflate the menu; this adds items to the action bar if it is present.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    // OAuth authenticated successfully, launch primary authenticated activity
+    // i.e Display application "homepage"
+    @Override
+    public void onLoginSuccess() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    // OAuth authentication flow failed, handle the error
+    // i.e Display an error dialog or toast
+    @Override
+    public void onLoginFailure(Exception e) {
+        e.printStackTrace();
+    }
+
+    // Click handler method for the button used to start OAuth flow
+    // Uses the client to initiate OAuth authorization
+    // This should be tied to a button used to login
+    public void loginToRest(View view) {
+        animatedView.startAnimation(zoomin);
+        getClient().connect();
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
 }
